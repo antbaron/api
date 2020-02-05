eb92f942459ffaa4e083ed68c34056e1b9ef18cb
sullyvan <s.armangau@laposte.net>
2020-02-05 12:33:04 +0100

    tests

--- apiBack/src/main/java/fr/epsi/api/user/User.java
+++ apiBack/src/main/java/fr/epsi/api/user/User.java
@@ -50,7 +50,7 @@ public class User {
 		}
 		return userDto;
 	}
-	
-	
+
+
 
 }
--- apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
+++ apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
@@ -1,16 +1,25 @@
 package fr.epsi.api.user;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import fr.epsi.api.security.SecurityService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.support.SecurityContextProvider;
+import sun.plugin.liveconnect.SecurityContextHelper;
+
+import javax.ws.rs.core.SecurityContext;
+
+import static org.mockito.ArgumentMatchers.anyString;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(JUnitPlatform.class)
@@ -18,6 +27,9 @@ class UserServiceImplTest {
 
 	@Mock
 	UserRepository userRepository;
+
+	@Mock
+	SecurityService securityService;
 	
 	@InjectMocks
 	public UserServiceImpl sut;
@@ -31,6 +43,46 @@ class UserServiceImplTest {
 		Iterable<User> result = sut.findAll();
 		//Assert
 		Assertions.assertEquals(users, result, "No user");
+	}
+
+	@Test
+	void testLogin() {
+		User user = new User();
+		user.setPseudo("Taliraz");
+		user.setPassword("crypt");
+		Optional<User> userO = Optional.of(user);
+		Mockito.doReturn(userO).when(userRepository).findById(ArgumentMatchers.eq("Taliraz"));
+		Mockito.doReturn("password").when(securityService).decryptPassword(
+				ArgumentMatchers.eq("crypt"),
+				anyString()
+		);
+		sut.login("Taliraz","password");
+		Mockito.verify(securityService).decryptPassword(ArgumentMatchers.eq("crypt"),anyString());
+	}
+
+	@Test
+	void testSave() throws UnsupportedEncodingException {
+		ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+		Mockito.doReturn("crypt").when(securityService).encryptPassword(anyString(),anyString());
+		Mockito.doReturn(null).when(userRepository).save(userCaptor.capture());
+		sut.save("Taliraz","password");
+		User userSaved = userCaptor.getValue();
+		User user = new User();
+		user.setPseudo("Taliraz");
+		user.setPassword("crypt");
+		Assertions.assertEquals("Taliraz",userSaved.getPseudo());
+		Assertions.assertEquals("crypt",userSaved.getPassword());
+	}
+
+	@Test
+	void testFind(){
+		User user = new User();
+		user.setPseudo("Taliraz");
+		user.setPassword("crypt");
+		Optional<User> userO = Optional.of(user);
+		Mockito.doReturn(userO).when(userRepository).findById(ArgumentMatchers.eq("Taliraz"));
+		User userReturned = sut.find("Taliraz");
+		Assertions.assertEquals(user,userReturned);
 	}
 
 }
