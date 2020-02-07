45cf28ac96e6c141cc6db583941a01d936d20c7b
Adil Sensei <adile>
2020-02-07 16:42:23 +0100

    commit 07/02

--- apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
+++ apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
@@ -2,6 +2,8 @@ package fr.epsi.api.user;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,5 +34,20 @@ class UserServiceImplTest {
 		//Assert
 		Assertions.assertEquals(users, result, "No user");
 	}
-
+	
+	/*
+	 * @Test void testFind() { //Arrange User user = new User();
+	 * Mockito.doReturn(user).when(userRepository).findAll(); //Act Iterable<User>
+	 * result = sut.findAll(); //Assert Assertions.assertEquals(users, result,
+	 * "No user"); }
+	 */
+	
+	//Mockito.verify(userRepository,Mockito.times(1).save(...));
+	//ArgumentCaptor<User> ac = ArgumentCaptor.forClass(user.class);
+	//Mockito.doReturn(null).when(userRepository).save(ac.capture());
+	//ac.getValue()
+	//Optional<User> ouser
+	//if(ouser.isPresent())
+	//Optional.of(new USer())
+	//Optional.empty();
 }
