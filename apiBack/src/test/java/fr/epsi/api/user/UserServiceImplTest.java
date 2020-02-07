786dc55a7ab972183d382ea39338735faf3e8839
lewislalande <lewislalande@gmail.com>
2020-02-07 10:45:07 +0100

    t

--- apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
+++ apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
@@ -1,12 +1,16 @@
 package fr.epsi.api.user;
 
+import fr.epsi.api.security.SecurityService;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import static org.mockito.ArgumentMatchers.anyString;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -19,6 +23,9 @@ class UserServiceImplTest {
 	@Mock
 	UserRepository userRepository;
 	
+        @Mock
+        SecurityService securityService;
+        
 	@InjectMocks
 	public UserServiceImpl sut;
 	
@@ -32,5 +39,50 @@ class UserServiceImplTest {
 		//Assert
 		Assertions.assertEquals(users, result, "No user");
 	}
+        
+        @Test
+        void testFindUser() {
+        //Arrange
+        User user = new User();
+        user.setPseudo("Test");
+        Mockito.doReturn(Optional.of(user)).when(userRepository).findById(user.getPseudo());
+        //Act
+        User result = sut.find("Test");
+
+        //Assert
+        Assertions.assertEquals(user, result, "User found");
+        }
+        @Test
+        void testLogin(){
+        //Arrange
+        User user = new User();
+        user.setPseudo("test");
+        user.setPassword("pwd");
+        Mockito.doReturn(Optional.of(user)).when(userRepository).findById(user.getPseudo());
+
+        Mockito.doReturn("password")
+                .when(securityService)
+                .decryptPassword(ArgumentMatchers.eq("pwd"), anyString());
+
+        //Act
+        sut.login("test", "password");
+        //Assert
+        Mockito.verify(securityService).decryptPassword(ArgumentMatchers.eq("pwd"), anyString());
+
+        }
+        
+        @Test
+        void testSave(){
+            //Arrange
+            //Act
+            //Assert
+        }
+        
+        @Test
+        void testFind(){
+            //Arrange
+            //Act
+            //Assert
+        }
 
 }
