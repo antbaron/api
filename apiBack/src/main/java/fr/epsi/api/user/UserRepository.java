90fc8e12798849edfb95aaae316ba7a70f9567b5
Hadrix <hadrien.huret@epsi.fr>
2020-02-07 16:40:20 +0100

    ajout de findByPassword

--- apiBack/src/main/java/fr/epsi/api/user/UserRepository.java
+++ apiBack/src/main/java/fr/epsi/api/user/UserRepository.java
@@ -3,7 +3,10 @@ package fr.epsi.api.user;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
 
+    List<User> findByPassword(String password);
 }
