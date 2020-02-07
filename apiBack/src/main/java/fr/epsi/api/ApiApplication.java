1682a4b60a184da5a149d2764100448ae87e94fc
Bougna34 <nathan.milhe@epsi.fr>
2020-02-07 16:41:11 +0100

    Ajout d'un commentaire inutile

--- apiBack/src/main/java/fr/epsi/api/ApiApplication.java
+++ apiBack/src/main/java/fr/epsi/api/ApiApplication.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
-
+        
+    //Ceci est un commentaire pour pratiquer le git commit
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
