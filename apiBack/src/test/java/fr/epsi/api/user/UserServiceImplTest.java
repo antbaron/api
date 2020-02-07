c7a2cbe3c571acd3c126c4d7286c5c4ee3712565
Arthur Alastuey <ar.alastuey@gmail.com>
2020-02-07 16:39:33 +0100

    Ajout testSave

--- apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
+++ apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
@@ -1,16 +1,24 @@
 package fr.epsi.api.user;
 
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import fr.epsi.api.security.SecurityService;
+import static org.mockito.ArgumentMatchers.anyString;
+
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(JUnitPlatform.class)
@@ -22,6 +30,9 @@ class UserServiceImplTest {
 	@InjectMocks
 	public UserServiceImpl sut;
 	
+	@Mock
+	public SecurityService securityService;
+	
 	@Test
 	void testFindAll() {		
 		//Arrange
@@ -31,6 +42,45 @@ class UserServiceImplTest {
 		Iterable<User> result = sut.findAll();
 		//Assert
 		Assertions.assertEquals(users, result, "No user");
+		
+		
 	}
+	
+	@Test
+	void testSave() throws UnsupportedEncodingException {
+			//Arrange
+            ArgumentCaptor<User> ac = ArgumentCaptor.forClass(User.class);
+            Mockito.doReturn(null).when(userRepository).save(ac.capture());
+			Mockito.doReturn("Password").when(securityService).encryptPassword(ArgumentMatchers.eq("Password"), anyString());
+            //Act
+            sut.save("Sam", "Password");
+            //Assert
+            Mockito.verify(userRepository, Mockito.times(1)).save(ac.getValue());
+            Assertions.assertEquals("Sam", ac.getValue().getPseudo());
+            Assertions.assertEquals("Password", ac.getValue().getPassword());
+		
+	}
+	
+	@Test
+	void testFind() {
+
+       /* ArgumentCaptor<User> ac = ArgumentCaptor.forClass(User.class);
+        Mockito.doReturn(null).when(userRepository).save(ac.capture());
+		Mockito.doReturn("Password").when(securityService).encryptPassword(ArgumentMatchers.eq("Password"), anyString());
+		
+		sut.find("antbaron");
+		
+		Mockito.verify(mock)*/
+	}
+	
+	
+	
 
 }
+
+
+
+
+
+
+
