dc3da3e7a73bb5937c1622bcad3f11d5af7be983
Antoine <antog1999@live.fr>
2020-02-07 17:14:15 +0100

    ajout test findbyid + save

--- apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
+++ apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
@@ -1,16 +1,25 @@
 package fr.epsi.api.user;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import javax.crypto.SecretKey;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import fr.epsi.api.security.SecurityService;
+import net.bytebuddy.asm.Advice.Argument;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(JUnitPlatform.class)
@@ -18,6 +27,9 @@ class UserServiceImplTest {
 
 	@Mock
 	UserRepository userRepository;
+	
+	@Mock
+	SecurityService securityService;
 	
 	@InjectMocks
 	public UserServiceImpl sut;
@@ -33,4 +45,50 @@ class UserServiceImplTest {
 		Assertions.assertEquals(users, result, "No user");
 	}
 
+	@Test
+	void testLogin() {		
+		//Arrange
+//		User user = new User();
+//		String pseudo = "test123";
+//		String password = "test321";
+//		user.setPassword(password);
+//		user.setPseudo(pseudo);
+//		Mockito.doReturn(user).when(userRepository).login(pseudo, password);
+//		//Act
+//		Iterable<User> result = sut.findAll();
+//		//Assert
+//		Assertions.assertEquals(users, result, "No user");
+	}
+	@Test
+	void save() throws UnsupportedEncodingException {
+		
+		User useru = new User();
+		
+		useru.setPassword("test321");
+		useru.setPseudo("test123");
+		
+		ArgumentCaptor<User> acu = ArgumentCaptor.forClass(User.class);
+		
+		Mockito.doReturn(null).when(userRepository).save(acu.capture());
+		
+		Mockito.doReturn("test321").when(securityService).encryptPassword("test321", "My_S3cr3t");
+		
+		sut.save("test123", "test321" );
+				
+		Assertions.assertEquals(useru.getPassword(), acu.getValue().getPassword());
+		
+	}
+	
+	@Test
+	void testFindById() {		
+		//Arrange
+		User useru = new User();
+		
+		Optional<User> user = Optional.of(useru);
+		Mockito.doReturn(user).when(userRepository).findById("1");
+		
+		User result = sut.find("1");
+		Assertions.assertEquals(result,  useru);
+	
+	}
 }
