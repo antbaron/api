a2109feee73213a7a01e4bed4dd177b52d482e15
VENIN30 <karim.dahmani@epsi.fr>
2020-02-12 09:21:35 +0100

    initial commit

--- apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
+++ apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package fr.epsi.api.user;
 
+import fr.epsi.api.security.SecurityService;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
@@ -7,9 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,6 +21,9 @@ class UserServiceImplTest {
 
 	@Mock
 	UserRepository userRepository;
+        
+        @Mock
+        SecurityService securityService;
 	
 	@InjectMocks
 	public UserServiceImpl sut;
@@ -31,6 +37,37 @@ class UserServiceImplTest {
 		Iterable<User> result = sut.findAll();
 		//Assert
 		Assertions.assertEquals(users, result, "No user");
-	}
+        }
+        
+//        @Test
+//	void testLogin() {		
+//		//Arrange
+//		List<User> users = new ArrayList<User>();
+//		Mockito.doReturn(users).when(userRepository).findAll();
+//		//Act
+//		Iterable<User> result = sut.findAll();
+//		//Assert
+//		Assertions.assertEquals(users, result, "No user");
+//                
+//                void login(pseudo, password)
+//	}
 
+        
+        
+        @Test
+        void testSave(){
+            
+            ArgumentCaptor<User> ac = ArgumentCaptor.forClass(User.class);
+            //Arrange
+            Mockito.doReturn(null).when(userRepository).save(ac.capture());
+            //Mockito.doReturn(null).when(securityService).save(ac.capture());
+            
+            //Act
+            sut.save("pseudo","password");
+            
+            //Assert
+            Mockito.verify(sut).save("pseudo", "password");
+            Assertions.assertEquals(ac.getValue().getPseudo(), "pseudo");
+            Assertions.assertEquals(ac.getValue().getPassword(), "password");
+        }
 }
