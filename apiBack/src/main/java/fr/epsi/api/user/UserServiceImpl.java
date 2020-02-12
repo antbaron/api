472bd3c7782d24217b2fc36ed7965dc8a62838c4
thibault.demay <thibault.demay@hotmail.fr>
2020-02-12 09:57:18 +0100

    Début test save

--- apiBack/src/main/java/fr/epsi/api/user/UserServiceImpl.java
+++ apiBack/src/main/java/fr/epsi/api/user/UserServiceImpl.java
@@ -12,48 +12,55 @@ import fr.epsi.api.security.SecurityService;
 @Service
 public class UserServiceImpl implements UserService {
 
-	private static final String SECRET_KEY = "My_S3cr3t";
+    private static final String SECRET_KEY = "My_S3cr3t";
 
-	private UserRepository userRepository;
+    private UserRepository userRepository;
 
-	private SecurityService securityService;
+    private SecurityService securityService;
 
-	@Autowired
-	public UserServiceImpl(UserRepository userRepository, SecurityService securityService) {
-		this.userRepository = userRepository;
-		this.securityService = securityService;
-	}
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, SecurityService securityService) {
+        this.userRepository = userRepository;
+        this.securityService = securityService;
+    }
 
-	@Override
-	public Iterable<User> findAll() {
-		return userRepository.findAll();
-	}
+    @Override
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
 
-	@Override
-	public void login(String pseudo, String password) {
-		Optional<User> user = userRepository.findById(pseudo);
-		Assert.isTrue(
-				user.isPresent()
-						&& password.equals(securityService.decryptPassword(user.get().getPassword(), SECRET_KEY)),
-				"Not connected");
-	}
+    @Override
+    public void login(String pseudo, String password) {
+        Optional<User> user = userRepository.findById(pseudo);
+        // Assert.isTrue lève une exception si ce n'est pas présent
+        Assert.isTrue(
+                user.isPresent()
+                        && password.equals(securityService.decryptPassword(user.get().getPassword(), SECRET_KEY)),
+                "Not connected");
+    }
 
-	@Override
-	public void save(String pseudo, String password) throws UnsupportedEncodingException {
-		User user = new User();
-		user.setPseudo(pseudo);
-		user.setPassword(securityService.encryptPassword(password, SECRET_KEY));
-		userRepository.save(user);
-	}
+    @Override
+    public void save(String pseudo, String password) throws UnsupportedEncodingException {
+        User user = new User();
+        user.setPseudo(pseudo);
+        user.setPassword(securityService.encryptPassword(password, SECRET_KEY));
+        userRepository.save(user);
+    }
 
-	@Override
-	public User find(String pseudo) throws Exception {
-		if (userRepository.findById(pseudo).isPresent()){
-			return userRepository.findById(pseudo).get();
-		}
-		else{
-			throw new Exception("USER NOT FOUND");
-		}
-	}
+    @Override
+    public User find(String pseudo) throws Exception {
+        if (userRepository.findById(pseudo).isPresent()) {
+            return userRepository.findById(pseudo).get();
+        } else {
+            throw new Exception("USER NOT FOUND");
+        }
+    }
+
+// ON NE PEUT PAS NE PAS TESTER LE RETOUR DU GET : IL FAUT Y METTRE UN IF POUR GERER LE CAS OU ON RETOURNE RIEN
+//	@Override
+//	public User find(String pseudo) {
+//
+//		return userRepository.findById(pseudo).get();
+//	}
 
 }
--- apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
+++ apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
@@ -1,14 +1,16 @@
 package fr.epsi.api.user;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+import fr.epsi.api.security.AESSecurityService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -20,22 +22,58 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @RunWith(JUnitPlatform.class)
 class UserServiceImplTest {
 
-	@Mock
-	UserRepository userRepository;
-	
-	@InjectMocks
-	public UserServiceImpl sut;
-	
-	@Test
-	void testFindAll() {		
-		//Arrange
-		List<User> users = new ArrayList<User>();
-		Mockito.doReturn(users).when(userRepository).findAll();
-		//Act
-		Iterable<User> result = sut.findAll();
-		//Assert
-		Assertions.assertEquals(users, result, "No user");
-	}
+    private static final String SECRET_KEY = "My_S3cr3t";
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    AESSecurityService securityService;
+
+    @InjectMocks
+    public UserServiceImpl sut;
+
+    @Test
+    void testFindAll() {
+        //Arrange
+        List<User> users = new ArrayList<User>();
+        Mockito.doReturn(users).when(userRepository).findAll();
+        //Act
+        Iterable<User> result = sut.findAll();
+        //Assert
+        Assertions.assertEquals(users, result, "No user");
+    }
+
+    // TOUT CE QUI SORT, IL FAUT LE MOCKER
+    @Test
+    void testSaveOK() throws UnsupportedEncodingException {
+        //Arrange
+        String pseudo = "pseudo";
+        String password = "password";
+        String secret = "secret";
+        ArgumentCaptor<User> ac = ArgumentCaptor.forClass(User.class);
+        Mockito.doReturn(null).when(userRepository).findById(pseudo); // Pas trouvé, ok pour save
+        Mockito.doReturn("encryptedPassword").when(securityService).encryptPassword(password, secret);
+        //Act
+        sut.save(pseudo, password);
+        //Assert
+    }
+
+    // TOUT CE QUI SORT, IL FAUT LE MOCKER
+    @Test
+    void testSaveNOKEncodingException() throws UnsupportedEncodingException {
+        //Arrange
+        String pseudo = "pseudo";
+        String password = "password";
+        ArgumentCaptor<User> ac = ArgumentCaptor.forClass(User.class);
+        Mockito.doReturn(null).when(userRepository).findById(pseudo); // Pas trouvé, ok pour save
+        Mockito.doThrow(new UnsupportedEncodingException("ERROR HAS BEEN LAUNCHED")).when(securityService).encryptPassword(password, SECRET_KEY);
+        //Act & Assert
+
+        Assertions.assertThrows(UnsupportedEncodingException.class, () -> {
+            sut.save(pseudo, password);
+        });
+
+    }
 
 	@Test
 	void testLogin() {
