b6ba6f26ffe9490cfbc19516aa9a35fdf663c728
lewislalande <lewislalande@gmail.com>
2020-02-14 10:16:48 +0100

    Final commit

--- apiBack/src/main/java/fr/epsi/api/user/UserServiceImpl.java
+++ apiBack/src/main/java/fr/epsi/api/user/UserServiceImpl.java
@@ -12,47 +12,47 @@ import fr.epsi.api.security.SecurityService;
 @Service
 public class UserServiceImpl implements UserService {
 
-	private static final String SECRET_KEY = "My_S3cr3t";
+    private static final String SECRET_KEY = "PassCrypted";
 
-	private UserRepository userRepository;
+    private UserRepository userRepository;
 
-	private SecurityService securityService;
+    private SecurityService securityService;
 
-	@Autowired
-	public UserServiceImpl(UserRepository userRepository, SecurityService securityService) {
-		this.userRepository = userRepository;
-		this.securityService = securityService;
-	}
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, SecurityService securityService) {
+        this.userRepository = userRepository;
+        this.securityService = securityService;
+    }
 
-	@Override
-	public Iterable<User> findAll() {
-		return userRepository.findAll();
-	}
+    @Override
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
 
-	@Override
-	public void login(String pseudo, String password) {
-		Optional<User> user = userRepository.findById(pseudo);
-		Assert.isTrue(
-				user.isPresent()
-						&& password.equals(securityService.decryptPassword(user.get().getPassword(), SECRET_KEY)),
-				"Not connected");
-	}
+    @Override
+    public void login(String pseudo, String password) {
+        Optional<User> user = userRepository.findById(pseudo);
+        Assert.isTrue(
+                user.isPresent()
+                && password.equals(securityService.decryptPassword(user.get().getPassword(), SECRET_KEY)),
+                "Not connected");
+    }
 
-	@Override
-	public void save(String pseudo, String password) throws UnsupportedEncodingException {
-		User user = new User();
-		user.setPseudo(pseudo);
-		user.setPassword(securityService.encryptPassword(password, SECRET_KEY));
-		userRepository.save(user);
-	}
+    @Override
+    public void save(String pseudo, String password) throws UnsupportedEncodingException {
+        User user = new User();
+        user.setPseudo(pseudo);
+        user.setPassword(securityService.encryptPassword(password, SECRET_KEY));
+        userRepository.save(user);
+    }
 
-	@Override
-	public User find(String pseudo) {
-            if (userRepository.findById(pseudo).get() != null)
-		return userRepository.findById(pseudo).get();
-            else {
-                return null;
-            }
-	}
+    @Override
+    public User find(String pseudo) {
+        try {
+            return userRepository.findById(pseudo).get();
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
 }
--- apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
+++ apiBack/src/test/java/fr/epsi/api/user/UserServiceImplTest.java
@@ -24,28 +24,28 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @RunWith(JUnitPlatform.class)
 class UserServiceImplTest {
 
-	@Mock
-	UserRepository userRepository;
-	
-        @Mock
-        SecurityService securityService;
-        
-	@InjectMocks
-	public UserServiceImpl sut;
-	
-	@Test
-	void testFindAll() {		
-            //Arrange
-            List<User> users = new ArrayList<User>();
-            Mockito.doReturn(users).when(userRepository).findAll();
-            //Act
-            Iterable<User> result = sut.findAll();
-            //Assert
-            Assertions.assertEquals(users, result, "No user");
-	}
-        
-        @Test
-        void testFindUserOk() {
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    SecurityService securityService;
+
+    @InjectMocks
+    public UserServiceImpl sut;
+
+    @Test
+    void testFindAll() {
+        //Arrange
+        List<User> users = new ArrayList<User>();
+        Mockito.doReturn(users).when(userRepository).findAll();
+        //Act
+        Iterable<User> result = sut.findAll();
+        //Assert
+        Assertions.assertEquals(users, result, "No user");
+    }
+
+    @Test
+    void testFindUserOk() {
         //Arrange
         User user = new User();
         Mockito.doReturn(Optional.of(user)).when(userRepository).findById("test");
@@ -54,52 +54,111 @@ class UserServiceImplTest {
 
         //Assert
         Assertions.assertEquals(user, result, "User found");
-        }
-        
-        @Test
-        void testFindUserKo() {
+    }
+
+    @Test
+    void testFindUserKo() {
         //Arrange
-        Mockito.doReturn(null).when(userRepository).findById("test");
+        Mockito.doReturn(Optional.empty()).when(userRepository).findById("toto");
         //Act
         User result = sut.find("test");
         //Assert
-        Assertions.assertEquals(null, result, "User not found");
-        }
-        
-        @Test
-        void testLoginOk(){
+        Assertions.assertNull(result);
+    }
+
+    @Test
+    void testLoginOk() {
         //Arrange
         User user = new User();
         user.setPassword("pwd");
         Mockito.doReturn(Optional.of(user)).when(userRepository).findById("test");
-        
+
         Mockito.doReturn("password")
                 .when(securityService)
                 .decryptPassword(ArgumentMatchers.eq("pwd"), anyString());
 
         //Act
-        sut.login("test","password");
+        sut.login("test", "password");
         //Assert
         Mockito.verify(securityService).decryptPassword(ArgumentMatchers.eq("pwd"), anyString());
-        }
-        
-        @Test
-        void testSave() throws UnsupportedEncodingException{
-          User user = new User();
-          user.setPseudo("test");
-          user.setPassword("pwd");
+    }
 
-          ArgumentCaptor<User> argumentCaptor = ArgumentCaptor.forClass(User.class);
-          Mockito.doReturn(user).when(userRepository).save(argumentCaptor.capture());
-          Mockito.doReturn("pwdSecure")
-                          .when(securityService)
-                          .encryptPassword(ArgumentMatchers.eq("pwd"),anyString());
+    @Test
+    void testLoginKO() {
+        //Arrange
+        User user = new User();
+        user.setPseudo("Test");
+        user.setPassword("Pass");
+        String passCrypted = "PassCrypted";
+        Mockito.doReturn(Optional.of(user))
+                .when(userRepository)
+                .findById(user.getPseudo());
+        Mockito.doReturn(user.getPassword())
+                .when(securityService)
+                .decryptPassword(user.getPassword(), passCrypted);
+        //Act
+        Assertions.assertThrows(IllegalArgumentException.class, () -> sut.login("Test", "password"));
+        //Assert
+        Mockito.verify(securityService).decryptPassword(user.getPassword(), passCrypted);
+    }
 
-          sut.save(user.getPseudo(),user.getPassword());
+    @Test
+    void testSave() throws UnsupportedEncodingException {
+        User user = new User();
+        user.setPseudo("test");
+        user.setPassword("pwd");
 
-          Mockito.verify(userRepository).save(argumentCaptor.getValue());
+        ArgumentCaptor<User> argumentCaptor = ArgumentCaptor.forClass(User.class);
+        Mockito.doReturn(user).when(userRepository).save(argumentCaptor.capture());
+        Mockito.doReturn("pwdSecure")
+                .when(securityService)
+                .encryptPassword(ArgumentMatchers.eq("pwd"), anyString());
 
-          Assertions.assertEquals(argumentCaptor.getValue().getPseudo(),"test");
-          Assertions.assertEquals(argumentCaptor.getValue().getPassword(),"pwdSecure");
-        }
+        sut.save(user.getPseudo(), user.getPassword());
+
+        Mockito.verify(userRepository).save(argumentCaptor.getValue());
+
+        Assertions.assertEquals(argumentCaptor.getValue().getPseudo(), "test");
+        Assertions.assertEquals(argumentCaptor.getValue().getPassword(), "pwdSecure");
+    }
+
+    @Test
+    void testSavePwdKO() throws UnsupportedEncodingException {
+        User user = new User();
+        user.setPseudo("test");
+        user.setPassword("pwd");
+
+        ArgumentCaptor<User> argumentCaptor = ArgumentCaptor.forClass(User.class);
+        Mockito.doReturn(user).when(userRepository).save(argumentCaptor.capture());
+        Mockito.doReturn("pwdSecure")
+                .when(securityService)
+                .encryptPassword(ArgumentMatchers.eq("pwd"), anyString());
+
+        sut.save(user.getPseudo(), user.getPassword());
+
+        Mockito.verify(userRepository).save(argumentCaptor.getValue());
+
+        Assertions.assertEquals(argumentCaptor.getValue().getPseudo(), "test");
+        Assertions.assertNotEquals(argumentCaptor.getValue().getPassword(), "Not the password");
+    }
+
+    @Test
+    void testSaveUserKO() throws UnsupportedEncodingException {
+        User user = new User();
+        user.setPseudo("test");
+        user.setPassword("pwd");
+
+        ArgumentCaptor<User> argumentCaptor = ArgumentCaptor.forClass(User.class);
+        Mockito.doReturn(user).when(userRepository).save(argumentCaptor.capture());
+        Mockito.doReturn("pwdSecure")
+                .when(securityService)
+                .encryptPassword(ArgumentMatchers.eq("pwd"), anyString());
+
+        sut.save(user.getPseudo(), user.getPassword());
+
+        Mockito.verify(userRepository).save(argumentCaptor.getValue());
+
+        Assertions.assertNotEquals(argumentCaptor.getValue().getPseudo(), "Not the pseudo");
+        Assertions.assertEquals(argumentCaptor.getValue().getPassword(), "pwdSecure");
+    }
 }
